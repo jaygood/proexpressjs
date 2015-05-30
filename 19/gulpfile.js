667750398a480e74c6b6bfa5ff21ca5c212e4718
Jon Haygood <jha@quartetfs.com>
2015-05-30 10:34:24 -0400

    Set up gulp and connect to api

--- 19/gulpfile.js
+++ 19/gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
-gulp.task('default', function () {
+gulp.task('build', ['build-js', 'build-html']);
+
+gulp.task('build-js', function () {
   return gulp.src('index.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-html', function () {
+  return gulp.src('./views/index.html')
+    .pipe(gulp.dest('./dist/views'));
+});
+
+gulp.task('default', ['build'], function () {
+  var app = require('./dist/index.js');
+  app.listen(3001);
 });
--- 19/index.js
+++ 19/index.js
@@ -1,14 +1,15 @@
 
+const express = require('express');
 const superagent = require('superagent');
 const consolidate = require('consolidate');
 
-const app = require('express')();
+const app = express();
 
 app.engine('html', consolidate.handlebars);
 app.set('view engine', 'html');
 app.set('views', `${__dirname}/views`);
 
-app.use(epress.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 const user = 'azat_co';
 const story_slug = 'kazan';
@@ -17,5 +18,15 @@ const username = '';
 const _token = '';
 
 app.get('/', (req, res) => {
-  superagent.get(`http://api.storify.com/v1/stories/${user}/${story_slug}`);
+  superagent.get(`http://api.storify.com/v1/stories/${user}/${story_slug}`)
+    .query({api_key, username, _token})
+    .set({Accept: 'application/json'})
+    .end((e, storifyResponse) => {
+      console.log('e', e, '\n--------------------------------------------------\n');
+      console.log('storify', storifyResponse.body.content, '\n--------------------------------------------------\n');
+      if (e) return next(e);
+      return res.render('index', storifyResponse.body.content);
+    });
 });
+
+module.exports = app;
